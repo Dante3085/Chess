030f0fb0305f93ce6df87a4960bfe7d7905aac04
Moritz Schäfer <moritz97.s@web.de>
2021-10-15 02:26:04 +0200

    BoardRenderer class skeleton

---
+++ BoardRenderer.py
@@ -0,0 +1,14 @@
+
+'''
+Um diese Klasse umzusetzen, muss ich erst noch mehr über PyGame wissen.
+Folgende Stichworte:
+- Game-Loop
+- Fenstersystem (Worauf soll gerendert werden?)
+'''
+
+class BoardRenderer:
+    def __init__(self, board) -> None:
+        self.board = board
+
+    def render(self, window):
+        pass
